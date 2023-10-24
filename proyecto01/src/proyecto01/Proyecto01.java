9cfb9b242e82d7e5d5376e262d63f3fd21eda582
Manuel Gomez <magomez36@utpl.edu.ec>
2023-10-23 21:41:34 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author sandr
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo basico de Java Versionado a traves de Git Manejado desde GitHub");
+    }
+    
+}
