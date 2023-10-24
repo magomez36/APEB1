50925f76dd1dbb725145d6b02e893b94a64d1e0d
Manuel Gomez <magomez36@utpl.edu.ec>
2023-10-23 22:29:45 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author sandr
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de Java versionado a traves de Git manejado desde GitHub ademas se usar GitHubClassroom");
+    }
+    
+}
